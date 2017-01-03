1173217f24baa0a78b2f529dff9fbd6d42149938
gernest <geofreyernest@live.com>
2017-01-03 15:40:26 +0300

    Implement subscription based streaming.

--- events/events.go
+++ events/events.go
@@ -1,5 +1,12 @@
 package events
 
+import (
+	"context"
+	"sync"
+
+	uuid "github.com/satori/go.uuid"
+)
+
 type Event struct {
 	Name string      `json:"name"`
 	Data interface{} `json:"data"`
@@ -7,14 +14,22 @@ type Event struct {
 
 type Stream struct {
 	evts chan *Event
+	subs map[string]chan *Event
+	mu   sync.RWMutex
 }
 
 func NewStream(size int) *Stream {
 	return &Stream{evts: make(chan *Event, size)}
 }
 
-func (s *Stream) Channel() <-chan *Event {
-	return s.evts
+func (s *Stream) Subscribe() (string, <-chan *Event) {
+	id := uuid.NewV4()
+	e := make(chan *Event, 10)
+	s.mu.Lock()
+	s.subs[id.String()] = e
+	s.mu.Unlock()
+	return id.String(), e
+
 }
 
 func (s *Stream) Send(evt *Event) {
@@ -23,4 +38,29 @@ func (s *Stream) Send(evt *Event) {
 
 func (s *Stream) send(evt *Event) {
 	s.evts <- evt
+}
+
+func (s *Stream) Start(ctx context.Context) {
+	go func() {
+		for {
+			select {
+			case ev := <-s.evts:
+				s.mu.RLock()
+				for _, ch := range s.subs {
+					go func(c chan *Event) {
+						ch <- ev
+					}(ch)
+				}
+				s.mu.RUnlock()
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+}
+
+func (s *Stream) Unsubscribe(id string) {
+	s.mu.Lock()
+	delete(s.subs, id)
+	s.mu.Unlock()
 }
