2f76f008795e65b4484ed463a2939837c0cac6e6
gernest <geofreyernest@live.com>
2017-01-04 14:34:59 +0300

    For update check if the imei and path match

--- db/db.go
+++ db/db.go
@@ -176,7 +176,7 @@ func UpdateDongle(db *sql.DB, d *Dongle) error {
 	  imei=$1,imsi=$2 ,path=$3,symlink=$4,
 	  tty=$5,properties=$6,
 	  created_on=$7 ,updated_on=now(),
-	  WHERE path=$3;
+	  WHERE path=$3&&imei=$1;
 	COMMIT;
 	`
 	var prop []byte
