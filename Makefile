e8e62526d9e35aefa393a2110aabed092833dae8
gernest <geofreyernest@live.com>
2017-01-25 14:31:14 +0300

    Bump version to 0.1.4

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.1.3
+VERSION=0.1.4
 NAME=devices_$(VERSION)
 OUT_DIR=bin/linux_arm/devices_$(VERSION)
 
--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 
 func main() {
 	app := cli.NewApp()
-	app.Version = "0.1.3"
+	app.Version = "0.1.4"
 	app.Usage = "Streams realtime events about devices (Dongles)"
 	app.Commands = []cli.Command{
 		{
